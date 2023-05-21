f174ed3a4d4f7d6203398d4a3af2167a83a7ce7e
mercenarioz <funnystar320@gmail.com>
2023-05-21 11:44:24 +0700

    Handle responsive header.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        'light-coffee': '#C89F94'
-      }
-    },
-  },
-  plugins: [],
-}
+    content: ['./public/**/*.{html,js}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                Karla: ['Karla', 'sans-serif'],
+            },
 
+            colors: {
+                'light-coffee': '#C89F94',
+            },
+
+            keyframes: {
+                slideDown: {
+                    '0%': { transform: 'translateY(-100%)' },
+                    '100%': { transform: 'translateY(0)' },
+                },
+            },
+
+            animation: {
+                slideDown: 'slideDown .15s ease-in-out',
+            },
+        },
+    },
+    plugins: [],
+}
